9a60fbb81f685b69141da92cb6eeeb6ed060e496
William Cesar Santos <william_cesar_santos@hotmail.com>
2026-01-08 20:58:12 -0300

    Exercicio dois

--- src/MainDate.java
+++ src/MainDate.java
@@ -23,4 +23,21 @@ public class MainDate {
         }
     }
 
+    // Faça um calculo de juros compostos.
+    // Você deve receber o valor inicial, a taxa de juros e a quantidade de meses.
+    // Imprima o saldo a cada mês, até que chegue na quantidade de meses.
+    public static void jurosCompostos(
+            float valorInicial,
+            float taxaJuros,
+            int meses
+    ) {
+        /**
+         * Saldo inicial: R$ 1.000,00
+         * Saldo após 1 mês: R$ 1.050,00, data 08/02/2024
+         * Saldo após 2 meses: R$ 1.102,50, data 08/03/2024
+         * Saldo após 3 meses: R$ 1.157,62, data 08/04/2024
+         * ...
+         */
+    }
+
 }
