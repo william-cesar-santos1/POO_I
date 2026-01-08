cf19749588dac90d00d01cd9fb91da90befe356c
William Cesar Santos <william_cesar_santos@hotmail.com>
2026-01-08 20:36:28 -0300

    Solução exercicio um

--- src/MainDate.java
+++ src/MainDate.java
@@ -1,6 +1,5 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 
 public class MainDate {
 
@@ -10,8 +9,18 @@ public class MainDate {
 
     public static void parcelas(int quantidade) {
         var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        var diaAtual  = LocalDate.now();
         // Calcule o vencimento das próximas parcelas, respeitando a quantidade
+        /**
+         * 1 vence dia 08/02/2024
+         * 2 vence dia 08/03/2024
+         * 3 vence dia 08/04/2024
+         * ...
+         */
+        var vencimento = LocalDate.now();
+        for (int index = 1; index <= quantidade; index++) {
+            vencimento = vencimento.plusMonths(1);
+            System.out.println("Parcela " + index + " vence dia " + formatter.format(vencimento));
+        }
     }
 
 }
